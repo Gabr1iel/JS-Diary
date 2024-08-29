cfedf08c97549bfd96b80b1a8651150d33afccee
Gabr1iel <137917783+Gabr1iel@users.noreply.github.com>
2024-08-29 04:43:19 +0200

    Transformation to diary
    inputs added,
    time manegment added,
    ls arr added,
    merging functions into showContent

--- script.js
+++ script.js
@@ -1,40 +1,113 @@
-const table = document.getElementById("infoTable");
-const keys = Object.keys(localStorage);
+const diaryWrapper = document.getElementById("diary-wrapper");
+let deleteBtn = document.createElement("button");
+let keys = Object.keys(localStorage);
+let lsLength = localStorage.length;
+let lsArr = [];
 
-function showItems() {
-    const keys = Object.keys(localStorage);
-    for(i = 0; i < localStorage.length; i++) {
-        const tr = document.createElement("tr");
-        const nameTd = document.createElement("td");
-        const buttonTd = document.createElement("td");
-        const deleteBtn = document.createElement("button");
-        deleteBtn.textContent = "Delete";
-        deleteBtn.classList.add("delete-btn");
-        deleteBtn.setAttribute(`id`, keys[i]);
-        deleteBtn.addEventListener("click", function() {
-            const keyId = deleteBtn.getAttribute("id");
+function updateValues() {
+    keys = Object.keys(localStorage);
+    getDateObject();
+    lsLength = localStorage.length;
+
+    
+    console.log(keys)
+    console.log(lsLength)
+    console.log(lsArr)
+}
+
+function rerenderContent(lsLength) {
+    for(let i = 0; i < lsLength; i++) {
+       let element = diaryWrapper.getElementsByClassName("diary-content")[0];
+
+       if (element) {
+        element.remove();
+       }
+    }
+}
+
+function showContent(lsLength) {
+    updateValues();
+    rerenderContent(lsLength);
+    for(let i = 0; i < localStorage.length; i++) {
+        const diary = localStorage.getItem(keys[i]);
+        const parsedDiary = JSON.parse(diary);
+        const a = parsedDiary.untilDate;
+
+        const dContent = document.createElement("div");
+        const p = document.createElement("p");
+        const h2 = document.createElement("h2");
+        const time = document.createElement("span");
+        const buttonWrapper = document.createElement("div");
+        const buttons = deleteBtn.cloneNode(true);
+        const diaryText = document.createElement("div");
+
+        dContent.classList.add("diary-content")
+        const result = countTime(a);
+        if (result <= 1) {
+            time.style.color = "red";
+        }
+        console.log(result);
+
+        buttons.textContent = "Delete";
+        buttons.classList.add("delete-btn");
+        buttons.setAttribute(`id`, keys[i]);
+        buttons.addEventListener("click", function() {
+            const lsLength = localStorage.length;
+            const keyId = buttons.getAttribute("id");
             localStorage.removeItem(keyId);
-        })
+            //deleteItems(lsLength);  
+            showContent(lsLength);
+        });
 
-        table.append(tr);
-        tr.append(nameTd);
-        tr.append(buttonTd);
-        nameTd.innerText = localStorage.getItem(keys[i]);
-        buttonTd.append(deleteBtn);
+        diaryWrapper.append(dContent)
+        dContent.append(diaryText);
+        dContent.append(buttonWrapper);
+        diaryText.append(h2);
+        diaryText.append(p);
+        diaryText.append(time);
+        h2.innerText = parsedDiary.title;
+        p.innerText = parsedDiary.description;
+        time.innerText = parsedDiary.untilDate;
+        buttonWrapper.append(buttons);
     }
     console.log(localStorage)
 }
 
-function deleteItems(lsLength) {
-    for(i = 0; i < lsLength; i++) {
-        table.getElementsByTagName("tr")[0].remove();
+function countTime(a) {
+    const date = new Date;
+    const year = date.getFullYear();
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const day = (date.getDate()).toString().padStart(2, '0');
+    const currentDate = `${year}${month}${day}`;
+    const finalDate = a.replace(/-/g, "")
+    const result = parseInt(finalDate) - parseInt(currentDate);
+
+    return result;
+}
+
+function getDateObject() {
+    lsArr.length = 0;
+    for (let i = 0; i < localStorage.length; i++) {
+        const ls = localStorage.getItem(keys[i]);
+        const parsedLs = JSON.parse(ls);
+        lsArr.push(parsedLs)
     }
+    const finalDates = lsArr.filter(obj => parseInt(obj.untilDate) > 1  ).map(obj => obj.untilDate);
+    const uniqueFinalDates = [...new Set(finalDates)]
+    console.log(uniqueFinalDates)
 }
 
 document.getElementById("form").addEventListener("submit", function(event) {
     event.preventDefault();
-    const name = document.getElementById("name").value;
-    const lsLength = localStorage.length;
+    const title = document.getElementById("titleInput");
+    const description = document.getElementById("descInput");
+    const inputDate = document.getElementById("dateInput");
+    const diary = {
+        title: title.value,
+        description: description.value,
+        untilDate: inputDate.value
+    }
+    //const lsLength = localStorage.length;
     const time = new Date;
     const date = {
         year: time.getFullYear(),
@@ -47,11 +120,63 @@ document.getElementById("form").addEventListener("submit", function(event) {
     };
     const dateKey = `${date.year}${date.month}${date.day}${date.hours}${date.minutes}${date.seconds}${date.milSeconds}`;
 
-    localStorage.setItem(dateKey, name);
-    deleteItems(lsLength);
-    showItems();
+    localStorage.setItem(dateKey, JSON.stringify(diary));
+    title.value = "";
+    description.value = "";
+    inputDate.value = "";
+    //deleteItems(lsLength);
+    showContent(lsLength);
 });
 
+
+
 document.addEventListener('DOMContentLoaded', function() {
-    showItems();
+    //updateValues();
+    showContent();
 });
+
+/**
+ for(let i = 0; i < localStorage.length; i++) {
+        const diary = localStorage.getItem(keys[i]);
+        const parsedDiary = JSON.parse(diary);
+        const a = parsedDiary.untilDate;
+
+        const dContent = document.createElement("div");
+        const p = document.createElement("p");
+        const h2 = document.createElement("h2");
+        const time = document.createElement("span");
+        const buttonWrapper = document.createElement("div");
+        const buttons = deleteBtn.cloneNode(true);
+        const diaryText = document.createElement("div");
+
+        dContent.classList.add("diary-content")
+        const result = countTime(a);
+        if (result <= 1) {
+            time.style.color = "red";
+        }
+        console.log(result);
+
+        buttons.textContent = "Delete";
+        buttons.classList.add("delete-btn");
+        buttons.setAttribute(`id`, keys[i]);
+        buttons.addEventListener("click", function() {
+            const lsLength = localStorage.length;
+            const keyId = buttons.getAttribute("id");
+            localStorage.removeItem(keyId);
+            //deleteItems(lsLength);  
+            showItems(lsLength);
+        });
+
+        diaryWrapper.append(dContent)
+        dContent.append(diaryText);
+        dContent.append(buttonWrapper);
+        diaryText.append(h2);
+        diaryText.append(p);
+        diaryText.append(time);
+        h2.innerText = parsedDiary.title;
+        p.innerText = parsedDiary.description;
+        time.innerText = parsedDiary.untilDate;
+        buttonWrapper.append(buttons);
+    }
+    console.log(localStorage)
+ */
